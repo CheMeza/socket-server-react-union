da2c76c3d90ddf085eb4318f8048e35cda7490ae
Che Meza <fabin.cds@gmail.com>
2022-03-01 16:08:08 -0800

    Path del socket server actualizado

--- models/server.js
+++ models/server.js
@@ -3,7 +3,9 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
-const Sockets = require('./sockets');
+const Sockets  = require('./sockets');
+const cors     = require('cors');
+
 
 class Server{
 
@@ -23,6 +25,8 @@ class Server{
     middlewares(){
         // Desplegar el directorio publico
         this.app.use(express.static( path.resolve(__dirname , '../public')));
+        //CORS
+        this.app.use(cors());
     }
 
     configurarSockets(){
